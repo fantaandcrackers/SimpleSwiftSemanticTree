e60a893083bbeb28889f4ccc926ce2be4d00155a
fantaandcrackers <fantaandcrackers@users.noreply.github.com>
2016-08-18 17:28:10 -0700

    Create EnumSemanticTree.swift

---
+++ EnumSemanticTree.swift
@@ -0,0 +1,21 @@
+//
+//  EnumSemanticTree.swift
+//  Created by Josh Basch on 8/17/16.
+//
+
+    enum expr {
+        case literal(Int)
+        case reference(String)
+        case add(expr, expr)
+        case sub(expr, expr)
+        case avg([expr])
+
+
+        func eval(context: [String:Int]) -> Int {
+            switch self {
+            case .literal(let val): return val
+            case .reference(let key): return context[key]
+            case .add(let lhs, let rhs): return lhs.eval(context) + rhs.eval(context)
+            }
+        }
+    }
